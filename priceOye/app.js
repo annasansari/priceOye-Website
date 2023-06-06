f81460c19a3b9f385b78e19bd2c65ef821a0cabf
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-06-06 20:49:09 +0500

    Add files via upload
    I MADE THIS USING HTML, CSS AND JAVASCRIPT

---
+++ priceOye/app.js
@@ -0,0 +1,366 @@
+var items = {
+    mobiles: {
+        oppo: {
+            oppoA16: {
+                Name: 'Oppo A16',
+                price: "Rs. 43,499",
+                locatio: "Karachi",
+                img: "images/Mobiles/Oppo/oppo-a16k.webp"
+            },
+            oppoA54: {
+                Name: 'Oppo A54',
+                price: "Rs. 56,299",
+                locatio: "Karachi",
+                img: "images/Mobiles/Oppo/oppo-a54.webp"
+            },
+            oppoA76: {
+                Name: 'Oppo A76',
+                price: "Rs. 64,899",
+                locatio: "Karachi",
+                img: "images/Mobiles/Oppo/oppo-a76.webp"
+            },
+            oppoA96: {
+                Name: 'Oppo A96',
+                price: "Rs. 72,999",
+                locatio: "Karachi",
+                img: "images/Mobiles/Oppo/oppo-a96.webp"
+            },
+            oppoF21: {
+                Name: 'Oppo F21',
+                price: "Rs. 101,999",
+                locatio: "Karachi",
+                img: "images/Mobiles/Oppo/oppo-f21-pro-5g.webp"
+            }
+        },
+        infinix: {
+            infinixHot11: {
+                Name: 'Infinx Hot 11',
+                price: "Rs. 34,199",
+                locatio: "Karachi",
+                img: "images/Mobiles/Infinix/infinix-hot-12-play.webp"
+            },
+            infinixHot12Play: {
+                Name: 'Infinx Hot 12 Play',
+                price: "Rs. 42,299",
+                locatio: "Karachi",
+                img: "images/Mobiles/Infinix/infinix-hot-11-play.webp"
+            },
+            infinixHot12I: {
+                Name: 'Infinx Hot 12i',
+                price: "Rs. 41,799",
+                locatio: "Karachi",
+                img: "images/Mobiles/Infinix/infinix-hot-12i.webp"
+            },
+            infinixNote12: {
+                Name: 'Infinix Note 12',
+                price: "Rs. 52,749",
+                locatio: "Karachi",
+                img: "images/Mobiles/Infinix/infinix-note-12.webp"
+            },
+            infinixSmart6: {
+                Name: 'Infinix Smart 6',
+                price: "Rs. 27,449",
+                locatio: "Karachi",
+                img: "images/Mobiles/Infinix/infinix-smart-6.webp"
+            }
+        },
+        samsung: {
+            samsungGalaxyA04: {
+                Name: 'Samsung Galaxy A04',
+                price: "Rs. 67,449",
+                locatio: "Karachi",
+                img: "images/Mobiles/Samsung/samsung-galaxy-a04.webp"
+            },
+            samsungGalaxyA13: {
+                Name: 'Samsung Galaxy A13',
+                price: "Rs. 57,449",
+                locatio: "Karachi",
+                img: "images/Mobiles/Samsung/samsung-galaxy-a13.webp"
+            },
+            samsungGalaxyA32: {
+                Name: 'Samsung Galaxy A32',
+                price: "Rs. 57,449",
+                locatio: "Karachi",
+                img: "images/Mobiles/Samsung/samsung-galaxy-a32.webp"
+            },
+            samsungGalaxyA33: {
+                Name: 'Samsung Galaxy A33',
+                price: "Rs. 78,449",
+                locatio: "Karachi",
+                img: "images/Mobiles/Samsung/samsung-galaxy-a33.webp"
+            },
+            samsungGalaxyA53: {
+                Name: 'Samsung Galaxy A53',
+                price: "Rs. 87,249",
+                locatio: "Karachi",
+                img: "images/Mobiles/Samsung/samsung-galaxy-a53.webp"
+            }
+        }
+    },
+    bikes: {
+        honda: {
+            hondaCb150: {
+                Name: 'Honda CB 150',
+                price: "Rs. 233,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Honda/honda-cb150f.webp"
+            },
+            hondaCg125: {
+                Name: 'Honda CD 70',
+                price: "Rs. 112,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Honda/honda-cd70.webp"
+            },
+            hondaCg125Self: {
+                Name: 'Honda CG 125 Self',
+                price: "Rs. 190,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Honda/honda-cg125 self.webp"
+            },
+            hondaCg125: {
+                Name: 'Honda CG 125',
+                price: "Rs. 200,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Honda/honda-cg125.webp"
+            },
+            hondaPridor: {
+                Name: 'Honda Pridor',
+                price: "Rs. 450,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Honda/honda-pridor.webp"
+            }
+        },
+        yamaha: {
+            yamahaYb120: {
+                Name: 'Yamaha Yb 120',
+                price: "Rs. 344,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Yamaha/yamaha-yb125.webp"
+            },
+            yamahaYbr: {
+                Name: 'Yamaha Ybr',
+                price: "Rs. 143,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Yamaha/yamaha-ybr.webp"
+            },
+            yamahaYbr125: {
+                Name: 'Yahmaha Ybr 125',
+                price: "Rs. 340,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Yamaha/yamaha-ybr125.webp"
+            },
+            yamahaYbr125G: {
+                Name: 'Yahama Ybr 125 G',
+                price: "Rs. 540,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Yamaha/yamaha-ybr125g.webp"
+            }
+        },
+        suzuki: {
+            suzukiGixxer: {
+                Name: 'Suzuki Gixxer',
+                price: "Rs. 646,500",
+                locatio: "Karachi",
+                img: "images/Bikes/Suzuki/suzuki-gixxer.webp"
+            },
+        }
+    },
+    laptops: {
+        macbook: {
+            appleMacbookAir: {
+                Name: 'Apple Macbook Air',
+                price: "Rs. 236,500",
+                locatio: "Karachi",
+                img: "images/Laptop/MacBook/apple-macbook-air-13-3-inches.webp"
+            },
+            appleMacbookAir13Inches: {
+                Name: 'Apple Macbook Air 13 Inches',
+                price: "Rs. 336,500",
+                locatio: "Karachi",
+                img: "images/Laptop/MacBook/apple-macbook-air-13-3.webp"
+            },
+            appleMacbookAirPro: {
+                Name: 'Apple Macbook Air Pro',
+                price: "Rs. 536,500",
+                locatio: "Karachi",
+                img: "images/Laptop/MacBook/apple-macbook-pro-13-3-inches-m1.webp"
+            },
+            appleMacbookAirPro16: {
+                Name: 'Apple Macbook Air Pro 16',
+                price: "Rs. 392,500",
+                locatio: "Karachi",
+                img: "images/Laptop/MacBook/apple-macbook-pro-16.webp"
+            },
+            appleMacbookAirPro13: {
+                Name: 'Apple Macbook Air Pro 13',
+                price: "Rs. 600,200",
+                locatio: "Karachi",
+                img: "images/Laptop/MacBook/apple-macbookk-pro-13.webp"
+            }
+        },
+        hpLaptops: {
+            hpChromeBook: {
+                Name: 'Hp Chrome Book',
+                price: "Rs. 200,200",
+                locatio: "Karachi",
+                img: "images/Laptop/Hp/hp-chromebook-x360-14.webp"
+            },
+            hpEg1inches: {
+                Name: 'Hp Eg 15 Inches',
+                price: "Rs. 120,200",
+                locatio: "Karachi",
+                img: "images/Laptop/Hp/hp-eg0503tx-15-6-inches.webp"
+            },
+            hpEq1inches: {
+                Name: 'Hp Eq 15 Inches',
+                price: "Rs. 110,200",
+                locatio: "Karachi",
+                img: "images/Laptop/Hp/hp-eq2179au-15-6-inches.webp"
+            },
+            hpFq1inches: {
+                Name: 'Hp Fq 20 Inches',
+                price: "Rs. 110,200",
+                locatio: "Karachi",
+                img: "images/Laptop/Hp/hp-fq5098tu-15-6.webp"
+            },
+            hpWictusGaming: {
+                Name: 'Hp Wictus Gaming',
+                price: "Rs. 360,700",
+                locatio: "Karachi",
+                img: "images/Laptop/Hp/hp-victus-gaming-laptop.webp"
+            }
+        },
+        lenovo: {
+            lenovoIdeaPad: {
+                Name: 'Lenovi Idea Pad',
+                price: "Rs. 142,700",
+                locatio: "Karachi",
+                img: "images/Laptop/Lenovo/lenovo-ideapad-3-15-6-inches.webp"
+            },
+            lenovoIdeaPad3: {
+                Name: 'Lenovi Idea Pad 3',
+                price: "Rs. 162,700",
+                locatio: "Karachi",
+                img: "images/Laptop/Lenovo/lenovo-ideapad-3.webp"
+            },
+            lenovoIdeaPadNew: {
+                Name: 'Lenovi IdeaPad New',
+                price: "Rs. 162,700",
+                locatio: "Karachi",
+                img: "images/Laptop/Lenovo/lenovo-ideapad.webp"
+            },
+            lenovoV15: {
+                Name: 'Lenovi V 15',
+                price: "Rs. 236,700",
+                locatio: "Karachi",
+                img: "images/Laptop/Lenovo/lenovo-v15-111g54-15-6-inches.webp"
+            },
+            lenovoYoga: {
+                Name: 'Lenovi Yoga',
+                price: "Rs. 546,700",
+                locatio: "Karachi",
+                img: "images/Laptop/Lenovo/lenovo-yoga-6.webp"
+            },
+        }
+    }
+}
+
+
+var allItems = document.getElementById('contents')
+
+function showAllItems() {
+    for (var mainItem in items) {
+        for (var catogry in items[mainItem]) {
+            for (var mob in items[mainItem][catogry]) {
+                allItems.innerHTML +=
+                    `
+                <div class="card col-sm-6 col-md-6 col-lg-4">
+                <img src=" ${items[mainItem][catogry][mob].img} "class="card-img-top" width="100px" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${items[mainItem][catogry][mob].Name}</h5>
+    <p class="card-text price">${items[mainItem][catogry][mob].price}</p>
+    <p class="card-text location">${items[mainItem][catogry][mob].locatio}</p>
+    <a href="#" class="btn btn-primary">Add to Cart</a>
+  </div>
+</div>`
+            }
+        }
+    }
+}
+showAllItems()
+
+function itemName(itemType) {
+    allItems.innerHTML = "";
+    if (itemType === "mobiles") {
+        allItems.innerHTML += `
+    <div class="priceOye-png">
+<i class="fa-solid fa-mobile mob-icon"></i>
+</div>
+    
+    `
+    }
+    else if (itemType === "laptops") {
+        allItems.innerHTML = `
+      <div class="priceOye-png">
+      <i class="fa-sharp fa-solid fa-laptop mob-icon"></i>
+      </div>
+      `
+
+    }
+    else if (itemType === "bikes") {
+        allItems.innerHTML = `
+      <div class="priceOye-png">
+      <i class="fa-sharp fa-solid fa-motorcycle mob-icon"></i>
+      </div>
+      `
+    //     var textColor = document.getElementById("text-color")
+    //     if (itemType === "bikes") {
+    //         textColor.style.color = "orange"
+    //     }
+    //     else {
+    //         textColor.style.color = "grey"
+    //     }
+    }
+
+    for (var key in items[itemType]) {
+        for (var type in items[itemType][key]) {
+            allItems.innerHTML += `
+            <div class="card col-sm-6 col-md-6 col-lg-4">
+                <img src=" ${items[itemType][key][type].img} "class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${items[itemType][key][type].Name}</h5>
+    <p class="card-text price">${items[itemType][key][type].price}</p>
+    <p class="card-text location">${items[itemType][key][type].locatio}</p>
+    <a href="#" class="btn btn-primary">Add to Cart</a>
+  </div>
+</div>
+            `
+        }
+    }
+}
+
+function itemsCatName(itemType, type) {
+    allItems.innerHTML = "";
+    allItems.innerHTML = `
+        <div class="priceOye-png">
+              <div class="text-style">  ${type} </div>
+        </div>
+`
+    for (var key in items[itemType][type]) {
+
+        allItems.innerHTML += `
+    <div class="card col-sm-6 col-md-6 col-lg-4">
+                <img src=" ${items[itemType][type][key].img} "class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${items[itemType][type][key].Name}</h5>
+    <p class="card-text price">${items[itemType][type][key].price}</p>
+    <p class="card-text location">${items[itemType][type][key].locatio}</p>
+    <a href="#" class="btn btn-primary">Add to Cart</a>
+  </div>
+</div>
+    `
+    
+    }
+}
+// itemsCatName()
+// itemName()
